acd5f293723f7abfd0be9704bcb3d0d5434de83c
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-03-21 22:20:01 +0530

    done almost

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 exports.dbconnect = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/movies")
+    .connect(process.env.DB_URL)
     .then(() => {
       console.log("db connect");
     })
